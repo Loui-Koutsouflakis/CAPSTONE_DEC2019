002896807bce1ed50d8e2c61a87a894ceb332b7c
Hamerite <DLRecovery@hotmail.com>
2019-01-23 14:26:32 -0500

    Update Dylan.cpp
    Does something now

--- Dylan.cpp
+++ Dylan.cpp
@@ -1,1 +1,14 @@
-//Stuff and Things
+#include <iostream>
+
+using namespace std;
+
+int first = 1;
+int second = 2;
+int result;
+
+int main()
+{
+	result = first + second;
+
+	cout << result << endl;
+}
