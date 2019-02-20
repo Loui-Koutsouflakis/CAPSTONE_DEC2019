a1d513790f0f5ae2e89b4017c6925ad741b1ca32
Benjamin.Young <Benjamin.Young@torontofilmschool.ca>
2019-02-19 21:36:47 -0500

    GameManagerForFrameworks

---
+++ GameManagerForFrameworks/LoadGameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadGameManager : MonoBehaviour
+{
+    public GameObject gameManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (GameManager.instance == null)
+            Instantiate(gameManager);
+    }
+}
