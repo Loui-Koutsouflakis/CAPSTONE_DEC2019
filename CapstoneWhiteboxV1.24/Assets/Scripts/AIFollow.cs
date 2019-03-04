2f2effcb628c822993732a60fd2d3282e591a692
najeebalmasri <45834007+najeebalmasri@users.noreply.github.com>
2019-03-04 11:58:50 -0500

    AIFollow Script
    AIFollow Script (basic)

---
+++ CapstoneWhiteboxV1.24/Assets/Scripts/AIFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIFollow : MonoBehaviour
+{
+    public Transform target;
+    public Transform myTransform;
+
+    void Update()
+    {
+
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+
+    }
+}
+}
