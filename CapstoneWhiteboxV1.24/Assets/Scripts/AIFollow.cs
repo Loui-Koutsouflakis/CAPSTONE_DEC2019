f8a2fb0003ad0062f0046948cc4eaf1376ff4144
najeebalmasri <45834007+najeebalmasri@users.noreply.github.com>
2019-03-04 12:29:14 -0500

    Update AIFollow.cs
    Updated

--- CapstoneWhiteboxV1.24/Assets/Scripts/AIFollow.cs
+++ CapstoneWhiteboxV1.24/Assets/Scripts/AIFollow.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class AIFollow : MonoBehaviour
 {
-    public Transform target;
+    public Transform target = player;
+    public Transform target2 = queen;
     public Transform myTransform;
+    public bool TargetLock = true;
 
     void Update()
     {
-
-        transform.LookAt(target);
-        transform.Translate(Vector3.forward * 2 * Time.deltaTime);
-
+        if (TargetLock == true)
+        {
+            transform.LookAt(player);
+            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+        }
+        else
+        {
+            transform.LookAt(queen);
+            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+        }
     }
 }
-}
+
