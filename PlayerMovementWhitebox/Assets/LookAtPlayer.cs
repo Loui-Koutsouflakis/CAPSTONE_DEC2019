137230072c6ecf0c791e158e0ab87a477cf851eb
Benjamin.Young <Benjamin.Young@torontofilmschool.ca>
2019-05-17 12:10:09 -0400

    GameMangerAndPlayerCombined
    Combined the game manager scenes /code with the player teams whitebox

---
+++ PlayerMovementWhitebox/Assets/LookAtPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour {
+
+
+    public GameObject camera;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(camera.transform);
+	}
+}
