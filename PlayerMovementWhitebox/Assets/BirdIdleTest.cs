06aadef89011c7d0a1c43c3fca9fb9873dbc8d30
Hamerite <DLRecovery@hotmail.com>
2019-05-17 11:08:58 -0400

    BirdIdleFollowTest
    - Looks in direction of player
    - Follows empty game objects position
    
    -Empty game object chooses new random point on a 2x2x2 grid at regular intervals

---
+++ PlayerMovementWhitebox/Assets/BirdIdleTest.cs
@@ -0,0 +1,38 @@
+﻿//Created by Dylan LeClair, May 17/19
+
+//Empty gameobject childed to the player must be created and tagged as "FollowPoint"
+//Player gameobject in scene must be tagged as "Player"
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdIdleTest : MonoBehaviour
+{
+    private GameObject idleFollowPoint;
+    private GameObject player;
+
+    private readonly float speed = 6;
+
+    private void Start()
+    {
+        idleFollowPoint = GameObject.FindGameObjectWithTag("FollowPoint");
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        LookDirection();
+        Follow();
+    }
+
+    private void LookDirection()
+    {
+        transform.LookAt(player.transform.position);
+    }
+
+    private void Follow()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, idleFollowPoint.transform.position, speed * Time.deltaTime);
+    }
+}
