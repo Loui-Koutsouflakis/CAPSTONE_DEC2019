5bab304c257c5f06dd4f54ad1a5c41f5112fe039
ATepylo <45440409+ATepylo@users.noreply.github.com>
2019-09-13 09:25:27 -0400

    More bug fixes in air movement
    Can now move in the air again.

--- LynxMaster/Assets/Scripts/Player/Player_Luke/MovementComponents/PlayerAirMovement.cs
+++ LynxMaster/Assets/Scripts/Player/Player_Luke/MovementComponents/PlayerAirMovement.cs
@@ -124,7 +124,14 @@ public class PlayerAirMovement : MonoBehaviour
         {
             if (!wallDeadZone)
             {
-                if(Vector3.Dot(forward, inputDir) < 0) // to prevent sticking to walls (and not slidining down) when input is in the direction of the wall
+                if (onWall)
+                {
+                    if (Vector3.Dot(forward, inputDir) < 0) // to prevent sticking to walls (and not slidining down) when input is in the direction of the wall
+                    {
+                        AirMovement();
+                    }
+                }
+                else
                 {
                     AirMovement();
                 }
