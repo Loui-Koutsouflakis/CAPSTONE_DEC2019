8978daf39b3a36383e4cdf003d38b5c285abb580
whitelilly883 <45440456+whitelilly883@users.noreply.github.com>
2019-10-09 15:37:34 -0400

    spider update
    got spider moving fluidly with rotation. Rather then stopping when reached destination spider rotates smoothly to face new direction while moving still.

--- Assets/Scripts/Enemies/SpiderMother.cs
+++ Assets/Scripts/Enemies/SpiderMother.cs
@@ -18,7 +18,7 @@ public class SpiderMother : MonoBehaviour
     Rigidbody rigidBody;
     float angleDifferenceForward = 0.0f;
     bool shootingTodds = false;
-    bool facingNewDirection = false;
+    bool facingNewDirection = true;
 
     private void Awake()
     {
@@ -31,6 +31,7 @@ public class SpiderMother : MonoBehaviour
     {
         locationSpawned = transform.position;
         SetDestination("Wonder");
+        facingNewDirection = false;
         //transform.
     }
 
@@ -46,29 +47,7 @@ public class SpiderMother : MonoBehaviour
     }
 
 
-    void Update()
-    {
-        if (kill) Kill();
-        //Debug.Log(GetComponent<Rigidbody>().velocity);
 
-        if(/*(transform.position - destination).magnitude < 1 && !enemyScript.seesPlayer*/facingNewDirection)
-        {
-            SetDestination("Wonder");
-        }
-        //Debug.Log(enemyScript.playerTrigger.transform.position);
-        if (enemyScript.playerTrigger != null)
-        {
-            if (enemyScript.seesPlayer && (locationSpawned - enemyScript.playerTrigger.transform.position).magnitude < range)
-            {
-                SetDestination("ChasePlayer");
-            }
-            else if (enemyScript.seesPlayer && (locationSpawned - enemyScript.playerTrigger.transform.position).magnitude > range)
-            {
-                SetDestination("StandAndShoot");
-            }
-        }
-        //Debug.Log((transform.position - destination).magnitude);   
-    }
 
 
 
@@ -79,11 +58,14 @@ public class SpiderMother : MonoBehaviour
         {
             if (MovementPattern == "Wonder")
             {
-                destination = nextDestination;
-                nextDestination = new Vector3(
+                destination = new Vector3(
                     Random.Range(locationSpawned.x - 20, locationSpawned.x + 20),
                     transform.position.y,
-                    Random.Range(locationSpawned.z - 20, locationSpawned.z + 20));
+                    Random.Range(locationSpawned.z - 20, locationSpawned.z + 20)); ;
+                //nextDestination = new Vector3(
+                //    Random.Range(locationSpawned.x - 20, locationSpawned.x + 20),
+                //    transform.position.y,
+                //    Random.Range(locationSpawned.z - 20, locationSpawned.z + 20));
             }
             else if (MovementPattern == "ChasePlayer")
             {
@@ -136,47 +118,134 @@ public class SpiderMother : MonoBehaviour
         shootingTodds = false;
     }
 
+    void Update()
+    {
+        //if((transform.position - destination).magnitude < 2 || )
+        //{
+        //    facingNewDirection = false;
+        //}
+        if (kill) Kill();
+        //Debug.Log(GetComponent<Rigidbody>().velocity);
+
+        if ((transform.position - destination).magnitude < 5 && !enemyScript.seesPlayer)
+        {
+            SetDestination("Wonder");
+        }
+        //Debug.Log(enemyScript.playerTrigger.transform.position);
+        if (enemyScript.playerTrigger != null)
+        {
+            if (enemyScript.seesPlayer && (locationSpawned - enemyScript.playerTrigger.transform.position).magnitude < range)
+            {
+                SetDestination("ChasePlayer");
+            }
+            else if (enemyScript.seesPlayer && (locationSpawned - enemyScript.playerTrigger.transform.position).magnitude > range)
+            {
+                SetDestination("StandAndShoot");
+            }
+        }
+        //Debug.Log((transform.position - destination).magnitude);   
+    }
+
+
+    float timeCount = 0f;
+    bool rotate = false;
     private void FixedUpdate()
     {
         //Vector3 movement = (destination - transform.position).normalized * (speed * Time.deltaTime);
         Vector3 movement = transform.forward * (speed * Time.deltaTime);
-        movement.y = 0.0f;
+        movement.y = 0;
 
-        if((transform.position - destination).magnitude < 2)
-        {
-
-        }
-
-        Vector3 targetPosition = rigidBody.position + movement;
-
-        // Calculate new desired rotation
-        Vector3 movementDirection = destination - rigidBody.position;
-        movementDirection.y = 0.0f;
-        movementDirection.Normalize();
+        Vector3 newDirection;
 
 
-        Vector3 currentFacingXZ = transform.forward;
-        currentFacingXZ.y = 0.0f;
+        //if (!facingNewDirection)
+        //{
+        //    rotate = true;
+        //}
+        Debug.Log(destination + " Help");
+        newDirection = destination - rigidBody.position;
+        newDirection.y = transform.forward.y;
+        float angle = Vector3.SignedAngle(newDirection, transform.forward, transform.up);
+        Debug.Log(angle + " angle");
 
-        angleDifferenceForward = Vector3.SignedAngle(movementDirection, currentFacingXZ, Vector3.up);
-        Vector3 targetAngularVelocity = Vector3.zero;
-        targetAngularVelocity.y = angleDifferenceForward * Mathf.Deg2Rad;
+        //if (angle > 5 || angle < -5)
+        //{
+        if (angle > 5) transform.Rotate(transform.up, -1);
+        if (angle < -5) transform.Rotate(transform.up, 1);
 
-        Quaternion syncRotation = Quaternion.identity;
-        syncRotation = Quaternion.LookRotation(movementDirection);
 
-        Vector3 temp = targetPosition;
-        temp.z += 0.1f;
-
-        rigidBody.MovePosition(targetPosition);
         //}
 
-        if (movement.sqrMagnitude > Mathf.Epsilon)
-        {
-            //Debug.Log("Meep");
-            // Currently we only update the facing of the character if there's been any movement
-            rigidBody.MoveRotation(syncRotation);
-        }
+        //if (rotate)
+        //{
+        //    newDirection = destination - rigidBody.position;
+        //    float angle = Vector3.Angle(newDirection, transform.forward);
+        //    //newDirection.y = 0.0f;
+        //    //newDirection.Normalize();
+        //    //Quaternion tempRotation = Quaternion.Slerp(Quaternion.Euler(transform.forward), Quaternion.Euler(newDirection), timeCount);
+        //    //tempRotation.
+        //    //Vector3 temp = tempRotation.eulerAngles;
+        //    //temp.x = 0;
+        //    //temp.z = 0;
+        //    //tempRotation.eulerAngles = temp;
+        //    transform.rotation = tempRotation;
+
+        //    timeCount += Time.deltaTime;
+
+        //}
+        //else
+        //{
+        //    timeCount = 0;
+        //    //newDirection = transform.forward;
+        //}
+
+
+        //if (facingNewDirection)
+        //{
+        //    newDirection = nextDestination - rigidBody.position;
+        //    newDirection.y = 0.0f;
+        //    newDirection.Normalize();
+        //}
+
+
+
+
+
+        //if(((newDirection * 10) - (transform.forward * 10)).magnitude < Mathf.Epsilon)
+        //{
+        //    facingNewDirection = true;
+        //    SetDestination("Wonder");
+        //}
+        Debug.Log(movement);
+        Vector3 targetPosition = rigidBody.position + transform.forward;
+
+
+        // Calculate new desired rotation
+
+
+
+        //Vector3 currentFacingXZ = transform.forward;
+        //currentFacingXZ.y = 0.0f;
+
+        //angleDifferenceForward = Vector3.SignedAngle(movementDirection, currentFacingXZ, Vector3.up);
+        //Vector3 targetAngularVelocity = Vector3.zero;
+        //targetAngularVelocity.y = angleDifferenceForward * Mathf.Deg2Rad;
+
+        //Quaternion syncRotation = Quaternion.identity;
+        //syncRotation = Quaternion.LookRotation(movementDirection);
+
+        //Vector3 temp = targetPosition;
+        //temp.z += 0.1f;
+        transform.position += (transform.forward * (speed * Time.deltaTime)); 
+        //rigidBody.MovePosition(targetPosition);
+        //}
+
+        //if (movement.sqrMagnitude > Mathf.Epsilon)
+        //{
+        //    //Debug.Log("Meep");
+        //    // Currently we only update the facing of the character if there's been any movement
+        //    rigidBody.MoveRotation(syncRotation);
+        //}
 
     }
 
