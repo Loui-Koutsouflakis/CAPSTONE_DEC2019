1e263beeb005409339a04733d8e8fe1d9380b595
whitelilly883 <45440456+whitelilly883@users.noreply.github.com>
2019-10-09 04:46:07 -0400

    Updated To 2019.2.6
    updated the project to 2019.2.6

---
+++ Assets/Scripts/Enemies/SpiderMother.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMother : MonoBehaviour
+{
+    public Enemy enemyScript;
+    public bool kill;
+    public float range;
+
+    public Vector3 locationSpawned;
+    public float speed;
+    private Vector3 destination;
+    private Vector3 nextDestination;
+    private WaitForSecondsRealtime frameRate = new WaitForSecondsRealtime(1/60);
+    Vector2 tempV2;
+    Vector2 tempLocation;
+    Rigidbody rigidBody;
+    float angleDifferenceForward = 0.0f;
+    bool shootingTodds = false;
+    bool facingNewDirection = false;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        enemyScript = GetComponent<Enemy>();
+
+    }
+
+    private void OnEnable()
+    {
+        locationSpawned = transform.position;
+        SetDestination("Wonder");
+        //transform.
+    }
+
+    private void Start()
+    {
+        
+    }
+    private void Kill()
+    {
+        StartCoroutine(enemyScript.DeathSequence());
+        this.enabled = false;
+        kill = false;
+    }
+
+
+    void Update()
+    {
+        if (kill) Kill();
+        //Debug.Log(GetComponent<Rigidbody>().velocity);
+
+        if(/*(transform.position - destination).magnitude < 1 && !enemyScript.seesPlayer*/facingNewDirection)
+        {
+            SetDestination("Wonder");
+        }
+        //Debug.Log(enemyScript.playerTrigger.transform.position);
+        if (enemyScript.playerTrigger != null)
+        {
+            if (enemyScript.seesPlayer && (locationSpawned - enemyScript.playerTrigger.transform.position).magnitude < range)
+            {
+                SetDestination("ChasePlayer");
+            }
+            else if (enemyScript.seesPlayer && (locationSpawned - enemyScript.playerTrigger.transform.position).magnitude > range)
+            {
+                SetDestination("StandAndShoot");
+            }
+        }
+        //Debug.Log((transform.position - destination).magnitude);   
+    }
+
+
+
+    // Current choices "Wonder" and "ChasePlayer"
+    public void SetDestination(string MovementPattern)
+    {
+        if (!shootingTodds)
+        {
+            if (MovementPattern == "Wonder")
+            {
+                destination = nextDestination;
+                nextDestination = new Vector3(
+                    Random.Range(locationSpawned.x - 20, locationSpawned.x + 20),
+                    transform.position.y,
+                    Random.Range(locationSpawned.z - 20, locationSpawned.z + 20));
+            }
+            else if (MovementPattern == "ChasePlayer")
+            {
+                destination = enemyScript.playerTrigger.transform.position;
+            }
+
+            float tempMag = 0;
+
+            if (MovementPattern != "StandAndShoot")
+            {
+                tempV2 = new Vector2(destination.x, destination.z);
+                tempLocation = new Vector2(locationSpawned.x, locationSpawned.z);
+                tempMag = (tempV2 - tempLocation).magnitude;
+            }
+            //Debug.Log(destination);
+            switch (MovementPattern)
+            {
+                case "Wonder":
+
+                    if (tempMag > range)
+                    {
+                        SetDestination(MovementPattern);
+                    }
+                    break;
+
+                case "ChasePlayer":
+                    if (tempMag > range)
+                    {
+                        SetDestination("StandAndShoot");
+                    }
+                    break;
+                case "StandAndShoot":
+                    destination = transform.position;
+                    if (!shootingTodds)
+                    {
+                        ShootTheTods();
+                        shootingTodds = true;
+                    }
+                    break;
+            }
+        }
+        tempV2 *= 0;
+        tempLocation *= 0;
+
+    }
+
+    void ShootTheTods()
+    {
+
+        shootingTodds = false;
+    }
+
+    private void FixedUpdate()
+    {
+        //Vector3 movement = (destination - transform.position).normalized * (speed * Time.deltaTime);
+        Vector3 movement = transform.forward * (speed * Time.deltaTime);
+        movement.y = 0.0f;
+
+        if((transform.position - destination).magnitude < 2)
+        {
+
+        }
+
+        Vector3 targetPosition = rigidBody.position + movement;
+
+        // Calculate new desired rotation
+        Vector3 movementDirection = destination - rigidBody.position;
+        movementDirection.y = 0.0f;
+        movementDirection.Normalize();
+
+
+        Vector3 currentFacingXZ = transform.forward;
+        currentFacingXZ.y = 0.0f;
+
+        angleDifferenceForward = Vector3.SignedAngle(movementDirection, currentFacingXZ, Vector3.up);
+        Vector3 targetAngularVelocity = Vector3.zero;
+        targetAngularVelocity.y = angleDifferenceForward * Mathf.Deg2Rad;
+
+        Quaternion syncRotation = Quaternion.identity;
+        syncRotation = Quaternion.LookRotation(movementDirection);
+
+        Vector3 temp = targetPosition;
+        temp.z += 0.1f;
+
+        rigidBody.MovePosition(targetPosition);
+        //}
+
+        if (movement.sqrMagnitude > Mathf.Epsilon)
+        {
+            //Debug.Log("Meep");
+            // Currently we only update the facing of the character if there's been any movement
+            rigidBody.MoveRotation(syncRotation);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [Header("Turn on/off the mouse cursor")]
+    public bool lockCursor = false;
+
+    [Header("Invert Camera Y (set to off for mouse control)")]
+    public bool invY;
+
+    [Header("Player")]
+    public GameObject Player;
+    public GameObject shadow;
+    Rigidbody p_RB;
+    Vector3 playerFront;
+
+    [Header("Camera Sensitivity and rotation smoothing(Suggested sensitivity = 4)")]
+    [Range(1, 10)]
+    public float sensitivity = 5;
+    [Range(0,1)]
+    public float rotationsmoothTime = 0.667f;
+    float yAngle = 75;
+
+    [Header("Distance from Player, Suggested Distance 2-3")]
+    [Range(1, 10)]
+    public float distFromPlayer = 3;
+    float properDistance = 3;
+    Vector2 pitchMinMax; //set to -15 and 90 respectively in editor
+
+    [Header("Debug current camera rotations and eulers")]
+    [SerializeField]
+    Vector3 currentRotation;
+    Vector3 eul;
+    Vector3 smoothingVelocity;
+    Vector3 temp;
+    float yaw;
+    float pitch;
+   // bool looking = false;
+
+    [Header("Camera Field of View")]
+    public float i_FOV = 60;
+    public float z_FOV = 30;
+    public float b_FOV = 80;
+    public float c_FOV;
+    public float smooth = 90;
+
+    [Header("CameraRayCast")]
+    public Camera main;
+    public GameObject aimer;
+    RaycastHit s_Ground;
+    LayerMask layerMask;
+    float cameraOffsetX = .2f;
+    float cameraOffsetY = 0.5f;
+
+    void Start()
+    {
+        main = Camera.main;
+        i_FOV = main.fieldOfView;
+        properDistance = distFromPlayer;
+        pitchMinMax = new Vector2(-5, 75);
+        properDistance = distFromPlayer;
+        p_RB = Player.GetComponent<Rigidbody>();
+
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+    private void Update()
+    {
+        c_FOV = main.fieldOfView;
+        if (ShadowManager(Player))
+        {
+            shadow.transform.position = new Vector3(s_Ground.point.x, s_Ground.point.y + 0.02f, s_Ground.point.z);
+        }
+    }
+    private void LateUpdate()
+    {
+        CamMovement();
+        JumpCamera();
+    }
+
+    void CamMovement()
+    {
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+        yaw += Input.GetAxis("CamX") * sensitivity + Input.GetAxis("MouseX") * sensitivity;
+        if (invY)
+        {
+            invertY();
+        }
+        if (!invY)
+        {
+            nonInvertY();
+        }
+        pitchMinMax = new Vector2(-12, yAngle);
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+        eul = transform.eulerAngles;
+        eul.x = 0;
+        transform.eulerAngles = currentRotation;
+        transform.position = Player.transform.position - (transform.forward - (transform.right * cameraOffsetX)) * CameraRaycast(distFromPlayer) + (transform.up * cameraOffsetY);
+
+    }
+
+    void invertY()
+    {
+        pitch += Input.GetAxis("CamY") * sensitivity + Input.GetAxis("MouseY") * sensitivity;
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref smoothingVelocity, rotationsmoothTime);
+
+    }
+    void nonInvertY()
+    {
+        pitch -= Input.GetAxis("CamY") * sensitivity + Input.GetAxis("MouseY") * sensitivity;
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref smoothingVelocity, rotationsmoothTime);
+    }
+
+    public void switchInverseY()
+    {
+        invY = !invY;
+    }
+
+    void JumpCamera()
+    {
+        
+        if (p_RB.velocity.y > 0 && c_FOV <= b_FOV)
+        {
+            main.fieldOfView -= (-smooth * Time.deltaTime);
+        }
+        if (p_RB.velocity.y <= 0 && c_FOV >= i_FOV)
+        {
+            main.fieldOfView += (-smooth * Time.deltaTime);
+        }
+    }
+
+    public bool ShadowManager(GameObject x)
+    {
+        int p_LayerMask = 1 << 9;
+        p_LayerMask = ~p_LayerMask;
+        Vector3 lineStart = x.transform.position;
+        Vector3 vectorToSearch = new Vector3(lineStart.x, lineStart.y - 10, lineStart.z);
+        return Physics.Linecast(lineStart, vectorToSearch, out s_Ground, p_LayerMask);
+    }
+
+    float CameraRaycast(float x)
+    {
+        int layerMask = 1 << 9;
+        layerMask = ~layerMask;
+        RaycastHit hit;
+        aimer.transform.LookAt(main.transform.position);
+        if (Physics.Raycast(aimer.transform.position, aimer.transform.TransformDirection(Vector3.forward), out hit,10, layerMask))
+        {
+            float dist = hit.distance;
+            if (dist < distFromPlayer)
+            {
+                x = dist;
+            }
+            else 
+            {
+                x = properDistance;
+            }
+        }
+        return x;
+    }
+    //*********************************************************************NOT IN USE BUT MAYBE LATER*************************************************************************
+    //void ZoomFunction()
+    //{
+    //    looking = true;
+    //    if (c_FOV != z_FOV)
+    //    {
+    //        if (c_FOV > z_FOV)
+    //        {
+    //            main.fieldOfView += (-smooth * Time.deltaTime);
+    //        }
+    //    }
+    //    else if (c_FOV <= z_FOV)
+    //    {
+    //        main.fieldOfView = z_FOV;
+    //    }
+    //}
+
+}
