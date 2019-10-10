33690cd884380ce9c870de912d27d162a7c28046
Benjamin Young <bly1354@gmail.com>
2019-10-10 18:03:47 -0400

    3D Menu Added
    Added the complete 3D menu with tony's assets

--- LynxMaster/Assets/Scripts/GameManagerAndUI/NewMainMenu.cs
+++ LynxMaster/Assets/Scripts/GameManagerAndUI/NewMainMenu.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewMainMenu : MonoBehaviour
+public class NewMainMenu : UIManager
 {
-    public GameObject[] MenuItems;
+    public Animation scrollOpen;
+    public GameObject[] MenuItems = new GameObject[3];
+     MenuButtons[] buttons = new MenuButtons[3];
     int selected = 0;
     public Material whenSelected;
     public Material whenNotSelected;
@@ -12,35 +15,72 @@ public class NewMainMenu : MonoBehaviour
     public float bufferTime;
     float verticalInput;
     bool recieveInput = true;
-    void Start()
+    public UIManager ui;
+
+
+
+    void Awake()
     {
+        InitilizeButtons();
+    }
+
+    void InitilizeButtons()
+    {
+
+
+        MenuItems[0].AddComponent<Button_StartGame>();
+        buttons[0] = MenuItems[0].GetComponent<Button_StartGame>();
+        Debug.Log(buttons[0]);
+        MenuItems[1].AddComponent<Button_Settings>();
+        buttons[1] = MenuItems[1].GetComponent<Button_Settings>();
+        MenuItems[2].AddComponent<Button_QuitGame>();
+        buttons[2] = MenuItems[2].GetComponent<Button_QuitGame>();
+
+
         
+
+      
     }
 
     void Update()
     {
+
+      
+
+
+
         verticalInput = Input.GetAxis("VerticalJoy") + Input.GetAxis("Vertical");
-
-        for (int i = 0; i < MenuItems.Length; i++)
+        if(scrollOpen.isPlaying == false)
         {
-            if(selected == i)
+
+            for (int i = 0; i < MenuItems.Length; i++)
             {
-                MenuItems[i].gameObject.GetComponent<Renderer>().material = whenSelected;
-            }
-            else
-            {
-                MenuItems[i].gameObject.GetComponent<Renderer>().material = whenNotSelected;
-            }
+                if(selected == i)
+                {
+                    MenuItems[i].gameObject.GetComponent<Renderer>().material = whenSelected;
+                    if (Input.GetButton("Jump"))
+                    {
+                        buttons[i].Execute(UIManager.singleton);
+                    }
+
+                    //buttons[i].Name();
+                }
+                else
+                {
+                    MenuItems[i].gameObject.GetComponent<Renderer>().material = whenNotSelected;
+                    //buttons[i].selected = false;
+                }
             
-        }
+            }
 
-        if(verticalInput > 0.9f && selected > 0 && recieveInput == true)
-        {
-            StartCoroutine("InputBufferSubract");
-        }
-        else if(verticalInput < -0.9f && selected < MenuItems.Length -1 && recieveInput == true)
-        {
-            StartCoroutine("InputBufferAdd");
+            if(verticalInput > 0.9f && selected > 0 && recieveInput == true)
+            {
+                StartCoroutine("InputBufferSubract");
+            }
+            else if(verticalInput < -0.9f && selected < MenuItems.Length -1 && recieveInput == true)
+            {
+                StartCoroutine("InputBufferAdd");
+            }
         }
     }
 
