611f448505ebd20583733c5c44670cc904d9d9f9
Benjamin Young <bly1354@gmail.com>
2019-10-10 13:14:03 -0400

    3D Menu Test
    Tested and created a primitive 3D Menu

---
+++ LynxMaster/Assets/Scripts/GameManagerAndUI/NewMainMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMainMenu : MonoBehaviour
+{
+    public GameObject[] MenuItems;
+    int selected = 0;
+    public Material whenSelected;
+    public Material whenNotSelected;
+    [Range(0f,1f)]
+    public float bufferTime;
+    float verticalInput;
+    bool recieveInput = true;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        verticalInput = Input.GetAxis("VerticalJoy") + Input.GetAxis("Vertical");
+
+        for (int i = 0; i < MenuItems.Length; i++)
+        {
+            if(selected == i)
+            {
+                MenuItems[i].gameObject.GetComponent<Renderer>().material = whenSelected;
+            }
+            else
+            {
+                MenuItems[i].gameObject.GetComponent<Renderer>().material = whenNotSelected;
+            }
+            
+        }
+
+        if(verticalInput > 0.9f && selected > 0 && recieveInput == true)
+        {
+            StartCoroutine("InputBufferSubract");
+        }
+        else if(verticalInput < -0.9f && selected < MenuItems.Length -1 && recieveInput == true)
+        {
+            StartCoroutine("InputBufferAdd");
+        }
+    }
+
+    private IEnumerator InputBufferSubract()
+    {
+        recieveInput = false;
+        selected--;
+        yield return new WaitForSeconds(bufferTime);
+        recieveInput = true;
+    }
+
+    private IEnumerator InputBufferAdd()
+    {
+        recieveInput = false;
+        selected++;
+        yield return new WaitForSeconds(bufferTime);
+        recieveInput = true;
+    }
+}
