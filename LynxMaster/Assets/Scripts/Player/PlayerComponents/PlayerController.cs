63eaf75740fb3d602c14cc130ca64279023b7cf6
ATepylo <45440409+ATepylo@users.noreply.github.com>
2019-10-11 17:40:40 -0400

    Removed second shadow and cleaned up playercontroller script

--- LynxMaster/Assets/Scripts/Player/PlayerComponents/PlayerController.cs
+++ LynxMaster/Assets/Scripts/Player/PlayerComponents/PlayerController.cs
@@ -155,7 +155,7 @@ public class PlayerController : MonoBehaviour
         {
             if (footHit.collider.gameObject != null && !isCrouching)
             {
-                if (footHit.collider.gameObject.tag == "MovingPlatform") //swtich to layer check not 
+                if (footHit.collider.gameObject.tag == "MovingPlatform") //swtich to layer check not tag
                 {
                     transform.parent = footHit.transform.parent;
                 }
@@ -181,6 +181,10 @@ public class PlayerController : MonoBehaviour
             }
             else if (footHit.collider.gameObject != null && isCrouching)
             {
+                if (footHit.collider.gameObject.tag == "MovingPlatform") //swtich to layer check not tag
+                {
+                    transform.parent = footHit.transform.parent;
+                }
                 player.SetGrounded(true);
                 player.SetFlutter(true);
                 player.SetMovementType(MovementType.crouch);
@@ -198,26 +202,7 @@ public class PlayerController : MonoBehaviour
                     psJump.Play();
                     jumpParticleIsPlaying = true;
                 }
-            }
-            //useless
-            //else if (footHit.collider.gameObject.tag == "MovingPlatform")
-            //{
-            //    transform.parent = footHit.transform.parent;
-            //    transform.parent = footHit.collider.gameObject.transform;
-            //    if (player.playerCurrentMove == MovementType.move)
-            //    {
-            //        psRun.Play();
-            //    }
-            //    else
-            //    {
-            //        psRun.Stop();
-            //    }
-            //    if (!jumpParticleIsPlaying)
-            //    {
-            //        psJump.Play();
-            //        jumpParticleIsPlaying = true;
-            //    }
-            //}
+            }          
         }
     }
 
