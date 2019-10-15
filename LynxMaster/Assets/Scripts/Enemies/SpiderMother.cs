dc19e1709d3ed0bede31aade077f7c2dc7bee80d
whitelilly883 <45440456+whitelilly883@users.noreply.github.com>
2019-10-15 05:00:46 -0400

    Spider Update
    Mother spider has
    -wonder, chase player, shoot spiderlings, attack player and vulnerable states.

--- LynxMaster/Assets/Scripts/Enemies/SpiderMother.cs
+++ LynxMaster/Assets/Scripts/Enemies/SpiderMother.cs
@@ -2,27 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpiderMother : MonoBehaviour
+public class SpiderMother : MonoBehaviour, IKillable
 {
-    public Enemy enemyScript;
+    [Range(0, 5)]
+    public int rotationSpeed = 3;
+
     public bool kill;
     public float range;
     public float meleeAttackRange;
-
-    public Vector3 locationSpawned;
+    public float shootRange;
     public float speed;
+    private WaitForSecondsRealtime vulnerableTime = new WaitForSecondsRealtime(5);
+    private WaitForSecondsRealtime timeBetweenSpiderlings = new WaitForSecondsRealtime(0.5f);
+    private WaitForSecondsRealtime shootCoolDownTimer = new WaitForSecondsRealtime(10);
+
+
     private Vector3 destination;
-    Vector2 tempV2;
-    Vector2 tempLocation;
+    Vector2 destinationVector2;
+    Vector2 tempLocationVector2;
     Rigidbody rigidBody;
-    float angleDifferenceForward = 0.0f;
     bool shootingTodds = false;
+
+    float timeCount = 0f;
+    bool rotate = false;
+    Vector3 movement;
+    float angle;
+    Vector3 newDirection;
+    private Enemy enemyScript;
+    private Vector3 locationSpawned;
+    public bool vulnerable = false;
+    bool attacking = false;
+    bool shootCoolDown = false;
 
     private void Awake()
     {
         rigidBody = GetComponent<Rigidbody>();
         enemyScript = GetComponent<Enemy>();
+    }
 
+    public IEnumerator CheckHit()
+    {
+        yield return 0;
+    }
+
+    public IEnumerator TakeDamage(int dmg)
+    {
+        enemyScript.TakeDamage(dmg);
+        yield return 0;
+    }
+
+    public IEnumerator Die()
+    {
+        enemyScript.DeathSequence();
+        yield return 0;
+    }
+
+    public void SetSpawnLocation(Vector3 pos)
+    {
+        locationSpawned = pos;
     }
 
     private void Kill()
@@ -32,31 +69,62 @@ public class SpiderMother : MonoBehaviour
         kill = false;
     }
 
-    // Current choices "Wonder" and "ChasePlayer"
+    IEnumerator ShootTheTods()
+    {
+        int numberOfSpiderlings = Random.Range(3, 6);
+        for (int i = 0; i < numberOfSpiderlings; i++)
+        {
+            Debug.Log("Shoot spiderling " + i);
+            yield return timeBetweenSpiderlings;
+        }
+
+        shootingTodds = false;
+        StartCoroutine(ShootCoolDown());
+    }
+
+    void AttackPlayer()
+    {
+        Debug.Log("Hit player");
+        Debug.Log("Play attack animation");
+        attacking = false;
+        vulnerable = true;
+        StartCoroutine(Valnerable());
+    }
+    private IEnumerator Valnerable()
+    {
+        Debug.Log("Play valnerable animation");
+
+        yield return new WaitForSecondsRealtime(10);
+
+        vulnerable = false;
+    }
+
+    private IEnumerator ShootCoolDown()
+    {
+
+
+        yield return new WaitForSecondsRealtime(10);
+
+        shootCoolDown = false;
+    }
+
+
     public void SetDestination(string MovementPattern)
     {
-        //if (!shootingTodds)
-        //{
-            if (MovementPattern == "Wonder")
-            {
-                destination.x = Random.Range(locationSpawned.x - 20, locationSpawned.x + 20);
-                destination.y = transform.position.y;
-                destination.z = Random.Range(locationSpawned.z - 20, locationSpawned.z + 20);
-            }
-            else if (MovementPattern == "ChasePlayer")
-            {
-                destination = enemyScript.playerTrigger.transform.position;
-                destination.y = transform.position.y;
-            }
+        float tempMag = 0;
 
-            float tempMag = 0;
-
+        if (!vulnerable)
+        {
             switch (MovementPattern)
             {
                 case "Wonder":
-                    tempV2 = new Vector2(destination.x, destination.z);
-                    tempLocation = new Vector2(locationSpawned.x, locationSpawned.z);
-                    tempMag = (tempV2 - tempLocation).magnitude;
+                    destination.x = Random.Range(locationSpawned.x - 20, locationSpawned.x + 20);
+                    destination.y = transform.position.y;
+                    destination.z = Random.Range(locationSpawned.z - 20, locationSpawned.z + 20);
+
+                    destinationVector2 = new Vector2(destination.x, destination.z);
+                    tempLocationVector2 = new Vector2(locationSpawned.x, locationSpawned.z);
+                    tempMag = (destinationVector2 - tempLocationVector2).magnitude;
                     if (tempMag > range)
                     {
                         shootingTodds = false;
@@ -65,79 +133,82 @@ public class SpiderMother : MonoBehaviour
                     break;
 
                 case "ChasePlayer":
-                    tempV2 = new Vector2(destination.x, destination.z);
-                    tempLocation = new Vector2(transform.position.x, transform.position.z);
-                    tempMag = (tempV2 - tempLocation).magnitude;
-                    if (tempMag > meleeAttackRange)
+                    destination = enemyScript.playerTrigger.transform.position;
+                    destination.y = transform.position.y;
+
+                    destinationVector2 = new Vector2(destination.x, destination.z);
+                    tempLocationVector2 = new Vector2(transform.position.x, transform.position.z);
+                    tempMag = (destinationVector2 - tempLocationVector2).magnitude;
+
+                    if (tempMag <= meleeAttackRange)
                     {
+                        attacking = true;
+                        AttackPlayer();
+                    }
+                    else if (tempMag > meleeAttackRange && tempMag > shootRange && !shootCoolDown)
+                    {
+                        shootCoolDown = true;
                         SetDestination("StandAndShoot");
                     }
+
                     break;
+
                 case "StandAndShoot":
-                    //destination = transform.position;
+                    destination = enemyScript.playerTrigger.transform.position;
                     if (!shootingTodds)
                     {
                         shootingTodds = true;
-                        ShootTheTods();
+                        StartCoroutine(ShootTheTods());
                     }
                     break;
             }
-        //}
-        tempV2 *= 0;
-        tempLocation *= 0;
-
-    }
-
-    void ShootTheTods()
-    {
-
-        shootingTodds = false;
+        }
+        else
+        {
+            destination = transform.position;
+        }
+        destinationVector2 *= 0;
+        tempLocationVector2 *= 0;
     }
 
     void Update()
     {
+        if (rotationSpeed > 5)
+        {
+            rotationSpeed = 5;
+            Debug.Log("Rotation speed to high. Rotation speed was set to 5.");
+        }
 
         if (kill) Kill();
 
-        if ((transform.position - destination).magnitude < 5 && !enemyScript.seesPlayer)
+        if (!enemyScript.seesPlayer && (transform.position - destination).magnitude < 5)
         {
             SetDestination("Wonder");
         }
 
-        if (enemyScript.playerTrigger != null)
+        if (enemyScript.seesPlayer)
         {
-            if (enemyScript.seesPlayer && (locationSpawned - enemyScript.playerTrigger.transform.position).magnitude < range)
-            {
-                SetDestination("ChasePlayer");
-            }
-            else if (enemyScript.seesPlayer && (locationSpawned - enemyScript.playerTrigger.transform.position).magnitude > range)
-            {
-                SetDestination("StandAndShoot");
-            }
+            SetDestination("ChasePlayer");
         }
     }
 
-
-    float timeCount = 0f;
-    bool rotate = false;
-    Vector3 movement;
-    float angle;
-    Vector3 newDirection;
     private void FixedUpdate()
     {
-        if (!shootingTodds)
+        if (!vulnerable && !attacking)
         {
-            movement = transform.forward * (speed * Time.deltaTime);
-            movement.y = 0;
-            transform.position += (transform.forward * (speed * Time.deltaTime));
+            if (!shootingTodds)
+            {
+                movement = transform.forward * (speed * Time.deltaTime);
+                movement.y = 0;
+                transform.position += (transform.forward * (speed * Time.deltaTime));
+            }
+
+            newDirection = destination - rigidBody.position;
+            newDirection.y = transform.forward.y;
+            angle = Vector3.SignedAngle(newDirection, transform.forward, transform.up);
+
+            if (angle > 5) transform.Rotate(transform.up, -rotationSpeed);
+            if (angle < -5) transform.Rotate(transform.up, rotationSpeed);
         }
-
-        newDirection = destination - rigidBody.position;
-        newDirection.y = transform.forward.y;
-        angle = Vector3.SignedAngle(newDirection, transform.forward, transform.up);
-
-        if (angle > 5) transform.Rotate(transform.up, -1);
-        if (angle < -5) transform.Rotate(transform.up, 1);
     }
-
 }
