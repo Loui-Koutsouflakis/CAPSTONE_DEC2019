dccb29eefa122022171bb8070a14766662ca43f3
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2019-11-14 19:07:47 -0500

    WIP Tutorial Functionality

--- Assets/NEW_DIR/Player/Script/Camera/CameraPaths.cs
+++ Assets/NEW_DIR/Player/Script/Camera/CameraPaths.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 [AddComponentMenu("Camera/Camera Path", 1)]
 public class CameraPaths : MonoBehaviour
 {
-
+    PlayerClass player;
     public bool active = false;
     public CPC_CameraPath path;
     public CPC_CameraPath[] paths;
     public float time = 10;
     public float stayTime;
+
+    [Header("If the object is collection based")]
+    [Tooltip("Crystal threshold")]
+    public float c_Threshold;
+
+    [Header("Animations?")]
+    [Tooltip("If your path requires an animation")]
+    public Animation anims;
+
     // Start is called before the first frame update
     void Start()
     {
+        player = FindObjectOfType<PlayerClass>();
         paths = FindObjectsOfType<CPC_CameraPath>();
+        if (anims == null)
+            Debug.Log("No big deal");
     }
 
     // Update is called once per frame
@@ -29,12 +41,17 @@ public class CameraPaths : MonoBehaviour
             path.gameObject.SetActive(true);
             StartCoroutine(goTime());
         }
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "Player")
-        active = true;
+        if (Vector3.Distance(player.gameObject.transform.position, transform.position) < 1 && this.gameObject.tag == "Obelisk")
+            if (Vector3.Distance(player.gameObject.transform.position, transform.position) < .2f)
+            {
+                if (player.GetShards() >= c_Threshold)
+                {
+                    active = true;
+                    if (anims != null)
+                        anims.Play();
+                }
+            }
     }
 
     IEnumerator goTime()
---
+++ Assets/ObeliskCollector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObeliskCollector : MonoBehaviour 
+{
+    PlayerClass player;
+    public CPC_CameraPath path;
+    public CPC_CameraPath[] paths;
+
+    public float c_Threshold;
+    bool didIt = false;
+    bool stopit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerClass>();
+        paths = FindObjectsOfType<CPC_CameraPath>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!stopit) {
+            if (Vector3.Distance(player.gameObject.transform.position, transform.position) < .2f)
+            {
+                if (player.GetShards() >= c_Threshold)
+                {
+                StartCoroutine(PlayAnim());
+                }
+            }
+        }
+    }
+
+    IEnumerator PlayAnim()
+    {
+        yield return new WaitForEndOfFrame();
+        //play animation
+        stopit = true;
+    }
+}
