8e583b8aa2149ccd4bbb45d8c8e2b4a152466ad3
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2019-11-22 15:44:36 -0500

    rEoRGANIZED COLLECTABLES
    *Added script for collectables spawning from plants
    *Added collectables to bring the total up to 100 in tutorial level

--- Assets/NEW_DIR/Player/Script/Camera/PlayerCamera.cs
+++ Assets/NEW_DIR/Player/Script/Camera/PlayerCamera.cs
@@ -26,7 +26,7 @@ public class PlayerCamera : MonoBehaviour
     [Tooltip("Adjusts camera movement sensitivity.")]
     [Range(1, 10)]
     public float sensitivity = 2.5f;
-    float r_Sensitivity = 3f;
+    float r_Sensitivity = 1f;
     float r_RotationSmoothTime = .1f;
     [Tooltip("Adjusts how quickly the camera moves acording to user input.")]
     [Range(0, 1)]
@@ -244,17 +244,17 @@ public class PlayerCamera : MonoBehaviour
     //Basic Camera Movements, Orbit around player and joystick control, as well as player offset
     void CamMovement3D()
     {
-        //if (p_RB.velocity.magnitude > 0)
-        //{
-        //    yaw += Input.GetAxis("HorizontalJoy") * r_Sensitivity + Input.GetAxis("Horizontal") * r_Sensitivity + Input.GetAxis("CamX") * sensitivity + Input.GetAxis("MouseX") * sensitivity;
-        //    currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref smoothingVelocity, r_RotationSmoothTime);
-        //}
-        //else
-        //{
+        if (p_RB.velocity.magnitude > 0.5f)
+        {
+            yaw += Input.GetAxis("HorizontalJoy") * r_Sensitivity + Input.GetAxis("Horizontal") * r_Sensitivity + Input.GetAxis("CamX") * sensitivity + Input.GetAxis("MouseX") * sensitivity;
+            currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref smoothingVelocity, r_RotationSmoothTime);
+        }
+        else
+        {
 
-        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref smoothingVelocity, rotationsmoothTime);
+            currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref smoothingVelocity, rotationsmoothTime);
             yaw += Input.GetAxis("CamX") * sensitivity + Input.GetAxis("MouseX") * sensitivity;
-        //}
+        }
         YAxis = Input.GetAxis("CamY") * sensitivity + Input.GetAxis("MouseY") * sensitivity;
         pitch = (invY) ? pitch += YAxis : pitch -= YAxis;
         pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
