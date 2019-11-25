276e75780c46b3cf4233bdc03772b8576d04a636
Loui-Koutsouflakis <Loui.Koutsouflakis@gmail.com>
2019-11-25 05:57:29 -0500

    Added Audio Scripts
    HandleMusic.cs - Create an object in the scene with this script to control music with
    
    HandleSfx.cs - Add this component to any sfx audio source in the game to control sfx with

---
+++ Assets/NEW_DIR/Debug/LouiDebugFolder_Please_Do_Not_Alter/Script/HandleMusic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleMusic : MonoBehaviour
+{
+    public AudioSource levelMusic;
+    public AudioSource enemyMusic;
+    public AudioSource deathMusic;
+
+    private readonly float musicDelay = 2f;
+    private readonly float deathMusicDelay = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(StartMusicDelay());
+    }
+
+    public void PlayLevelMusic()
+    {
+        deathMusic.Stop();
+        levelMusic.Stop();
+        levelMusic.Play();
+    }
+
+    public IEnumerator StartMusicDelay()
+    {
+        yield return new WaitForSeconds(musicDelay);
+
+        levelMusic.Play();
+    }
+
+    public IEnumerator CueDeathMusic()
+    {
+        levelMusic.Stop();
+        yield return new WaitForSeconds(deathMusicDelay);
+        deathMusic.Play();
+    }
+}
