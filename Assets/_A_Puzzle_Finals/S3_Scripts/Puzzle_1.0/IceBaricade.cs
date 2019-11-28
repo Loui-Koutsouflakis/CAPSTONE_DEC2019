d225ff4fe0f88b885ab5f2609cc1341c236408ed
Dylan LeClair <45439796+Hamerite@users.noreply.github.com>
2019-11-28 18:41:05 -0500

    Scene_3
    Some Sounds, temporary textures, placeholders, final camera functionality, enviroment objects added, and moved. Folders in Scene 3 folder re-organized. Moving platforms tweaked.
    
    Textures, finished and cut sounds, ground pound indicator, enemies needed.

---
+++ Assets/_A_Puzzle_Finals/S3_Scripts/Puzzle_1.0/IceBaricade.cs
@@ -0,0 +1,35 @@
+﻿//Created by Dylan LeClair 11/11/19
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceBaricade : MonoBehaviour
+{
+    private Animator animator;
+    private AudioSource source;
+
+    private float timer =5.0f;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlayBreak()
+    {
+        animator.SetBool("Break", true);
+
+        source.time = 0.7f;
+        source.PlayDelayed(1.2f);
+        StartCoroutine(CleanUp());
+    }
+
+    private IEnumerator CleanUp()
+    {
+        yield return new WaitForSecondsRealtime(timer);
+
+        gameObject.SetActive(false);
+    }
+}
