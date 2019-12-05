d25fe93736c92156d3255b8f70b34ae63f7f58fa
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2019-12-05 12:19:04 -0500

    Added Dylans changes to branch from f_Level

---
+++ Assets/_A_Puzzle_Finals/S3_Scripts/CreateMovingPlatform.cs
@@ -0,0 +1,30 @@
+﻿//Created by Dylan LeClair 11/11/19
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMovingPlatform : MonoBehaviour
+{
+    //Distiguish between active and inactive moving platforms
+    public GameObject movingPlatform;
+
+    //Camera path reference
+    private CameraPaths cameraPaths;
+
+    private void Start()
+    {
+        //Get camera path component
+        cameraPaths = GetComponent<CameraPaths>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Set moving platform active & start camera path for inactive moving platform
+        if (other.gameObject.layer == 14)
+        {
+            cameraPaths.StartMeUp();
+            movingPlatform.SetActive(true);
+        }
+    }
+}
---
+++ Assets/_A_Puzzle_Finals/S3_Scripts/Icicles.cs
@@ -0,0 +1,104 @@
+﻿//Created by Dylan LeClair 10/28/2019
+
+//Small icicle gameobject must have rigidbody attached
+//All icicles must have animator and audio source attached
+//Icicle reference must be set in inspector "Small or Large"
+
+//Player gameobject must be tagged "Player"
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Icicles : MonoBehaviour
+{
+    //Distiguish between large and small icicles
+    public enum IcicleIdetifier { small, large };
+    public IcicleIdetifier icicleSize;
+
+    //Object & component reference variables
+    private Animator animator;
+    private AudioSource source;
+    private GameObject playerRef;
+    private Rigidbody rigidbodyRef;
+
+    //Distance variables
+    private float distanceToPlayer;
+    private readonly float thresholdDistance = 20.0f;
+
+    private void Awake()
+    {
+        //Get rigidbody reference from small icicles
+        if(icicleSize == IcicleIdetifier.small)
+        {
+            rigidbodyRef = GetComponent<Rigidbody>();
+        }
+    }
+
+    private void Start()
+    {
+        //Get reference to player object && components
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        animator = GetComponent<Animator>();
+        source = GetComponent<AudioSource>();
+
+        if(rigidbodyRef)
+        {
+            //Keep icicles in the air
+            rigidbodyRef.useGravity = false;
+            rigidbodyRef.isKinematic = true;
+        }
+    }
+
+    private void Update()
+    {
+        //Keep track of distance from player to small icicles
+        distanceToPlayer = Vector3.Distance(transform.position, playerRef.transform.position);
+
+        //Check if small icicles should fall
+        if(icicleSize == IcicleIdetifier.small && distanceToPlayer < thresholdDistance)
+        {
+            //Call falling script
+            MakeIcicleFall();
+        }
+    }
+
+    public void MakeIcicleFall()
+    {
+        if(icicleSize == IcicleIdetifier.small)
+        {
+            //Drop small icicles
+            rigidbodyRef.useGravity = true;
+            rigidbodyRef.isKinematic = false;
+        }
+        if(icicleSize == IcicleIdetifier.large)
+        {
+            //Drop large icicles
+            animator.SetBool("Shake", true);
+
+            //Turn off script
+            this.enabled = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Play smashing animation and sound
+        if (icicleSize == IcicleIdetifier.small)
+        {
+            animator.SetBool("Smash", true);
+
+            source.Play();
+
+            StartCoroutine(CleanUp());
+        }
+    }
+
+    private IEnumerator CleanUp()
+    {
+        //Clean up small icicles after animation has played
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        gameObject.SetActive(false);
+    }
+}
