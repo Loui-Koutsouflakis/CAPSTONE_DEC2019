d43796a2d53eb18c72d44f4377dbd7cf9ef24bd9
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2019-12-12 15:41:11 -0500

    breakable ice in level 4

--- Assets/groundPoundPuzzle.cs
+++ Assets/groundPoundPuzzle.cs
@@ -2,17 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class groundPoundPuzzle : MonoBehaviour
+public class groundPoundPuzzle : MonoBehaviour, Interact
 {
+    MeshCollider[] mesh;
+    Rigidbody[] body;
+    ParticleSystem particles;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mesh = GetComponentsInChildren<MeshCollider>();
+        body = GetComponentsInChildren<Rigidbody>();
+        particles = GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void DontInteractWithMe()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void InteractWithMe()
+    {
+        particles.Stop();
+        foreach (var item in mesh)
+        {
+            item.convex = true;
+        }
+        foreach (var item in body)
+        {
+            item.useGravity = true;
+        }
+        StartCoroutine(DisableShit());
+    }
+
+    IEnumerator DisableShit()
+    {
+        yield return new WaitForSeconds(1);
+        foreach (var item in mesh)
+        {
+            item.convex = false;
+        }
+        GetComponent<BoxCollider>().enabled = false;
     }
 }
