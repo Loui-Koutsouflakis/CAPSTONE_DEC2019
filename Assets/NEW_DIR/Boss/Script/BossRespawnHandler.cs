b1c88732979503cbee165df2cccadde4f32c6f74
Loui Koutsouflakis <45789421+Loui-Koutsouflakis@users.noreply.github.com>
2019-12-15 04:55:23 -0500

    Boss scene update

--- Assets/NEW_DIR/Boss/Script/BossRespawnHandler.cs
+++ Assets/NEW_DIR/Boss/Script/BossRespawnHandler.cs
@@ -13,8 +13,6 @@ public class BossRespawnHandler : MonoBehaviour
 
     public IEnumerator Respawn()
     {
-        //Debug.Log("Respawn");
-
         respawning = true;
 
         StartCoroutine(transition.BlinkSequence(1.5f, 0.2f, 1.5f, 1f, false));
